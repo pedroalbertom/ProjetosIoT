84885466dfcb298026854c0a94927b7508abefb2
Pedro Alberto <pedroalbertompontes@gmail.com>
2024-02-09 15:24:08 -0300

    sei la

---
+++ Projetos Pessoais/Claw_receiver/.pio/build/esp32dev/FrameworkArduino/HWCDC.cpp.d
@@ -0,0 +1,4 @@
+.pio/build/esp32dev/FrameworkArduino/HWCDC.cpp.o: \
+ /home/pa/.platformio/packages/framework-arduinoespressif32/cores/esp32/HWCDC.cpp \
+ /home/pa/.platformio/packages/framework-arduinoespressif32/cores/esp32/USB.h \
+ /home/pa/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/dio_qspi/include/sdkconfig.h
---
+++ Projetos Pessoais/Claw_receiver/.pio/build/esp32dev/FrameworkArduino/stdlib_noniso.c.d
@@ -0,0 +1,12 @@
+.pio/build/esp32dev/FrameworkArduino/stdlib_noniso.c.o: \
+ /home/pa/.platformio/packages/framework-arduinoespressif32/cores/esp32/stdlib_noniso.c \
+ /home/pa/.platformio/packages/framework-arduinoespressif32/cores/esp32/stdlib_noniso.h \
+ /home/pa/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/esp_system/include/esp_system.h \
+ /home/pa/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/esp_common/include/esp_err.h \
+ /home/pa/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/esp_common/include/esp_compiler.h \
+ /home/pa/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/esp_common/include/esp_attr.h \
+ /home/pa/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/esp_common/include/esp_bit_defs.h \
+ /home/pa/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/esp_common/include/esp_idf_version.h \
+ /home/pa/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/esp_hw_support/include/esp_mac.h \
+ /home/pa/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/esp_hw_support/include/esp_chip_info.h \
+ /home/pa/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/esp_hw_support/include/esp_random.h
